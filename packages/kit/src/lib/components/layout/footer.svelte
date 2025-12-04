70a8b11fd7050f046c168049883e1e5a4a7f9c72
theetherGit <meenashivam9650@gmail.com>
2025-12-04 11:20:13 +0530

    fix(package): Use site name in footer instead of hardcoded `svecodocs`

--- packages/kit/src/lib/components/layout/footer.svelte
+++ packages/kit/src/lib/components/layout/footer.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import SocialIcons from "../social-icons.svelte";
 	import ThemeDropdown from "./theme-dropdown.svelte";
+	import { useSiteConfig } from "$lib/hooks/use-site-config.svelte.js";
 
 	const year = new Date().getFullYear();
+	const siteConfig = useSiteConfig()
 </script>
 
 <footer class="bg-background-secondary flex h-16 items-center gap-2 border-t px-4">
@@ -11,8 +13,10 @@
 		<a
 			href="https://github.com/svecosystem"
 			target="_blank"
-			class="underline-offset-2 hover:underline">Svecosystem</a
+			class="underline-offset-2 hover:underline"
 		>
+			{siteConfig.current.name}
+		</a>
 	</span>
 	<div class="ml-auto flex items-center">
 		<SocialIcons />
